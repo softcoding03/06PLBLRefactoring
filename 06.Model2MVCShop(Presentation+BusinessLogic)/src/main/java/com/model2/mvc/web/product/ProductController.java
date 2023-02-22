e08e716cabe59a1b23e0fc0ef225d41f60ed6c53
softcoding03 <didtn1233@gmail.com>
2023-02-22 11:04:04 +0900

    [CREAT]

---
+++ 06.Model2MVCShop(Presentation+BusinessLogic)/src/main/java/com/model2/mvc/web/product/ProductController.java
@@ -0,0 +1,147 @@
+package com.model2.mvc.web.product;
+
+
+import java.sql.Date;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.model2.mvc.common.Page;
+import com.model2.mvc.common.Search;
+import com.model2.mvc.service.domain.Product;
+import com.model2.mvc.service.domain.User;
+import com.model2.mvc.service.product.ProductService;
+import com.model2.mvc.service.user.UserService;
+
+
+//==> 회원관리 Controller
+@Controller
+public class ProductController {
+	
+	///Field
+	@Autowired
+	@Qualifier("productServiceImpl")
+	private ProductService productService;
+	//setter Method 구현 않음
+		
+	public ProductController(){
+		System.out.println(this.getClass());
+	}
+	
+	//==> classpath:config/common.properties  ,  classpath:config/commonservice.xml 참조 할것
+	//==> 아래의 두개를 주석을 풀어 의미를 확인 할것
+	//@Value("#{commonProperties['pageUnit']}")
+	@Value("#{commonProperties['pageUnit'] ?: 3}")
+	int pageUnit;
+	
+	//@Value("#{commonProperties['pageSize']}")
+	@Value("#{commonProperties['pageSize'] ?: 2}")
+	int pageSize;
+	
+	
+	@RequestMapping("/addProductView.do")
+	public String addProductView() throws Exception {
+
+		System.out.println("/addProductView.do");
+		
+		return "forward:/product/addProductView.jsp";
+	}
+	
+	@RequestMapping("/addProduct.do")
+	public String addProduct(@ModelAttribute("product") Product product, Model model) throws Exception {
+
+		System.out.println("/addProduct.do");
+		//Business Logic
+		productService.addProduct(product);
+		model.addAttribute("product", product);
+		
+		return "forward:/product/addProduct.jsp";
+	}
+	
+	@RequestMapping("/getProduct.do")
+	public String getProduct(@RequestParam("prodNo") int prodNo, Model model ) throws Exception {
+		
+		System.out.println("/getProduct.do");
+		System.out.println("prodNo int로 잘 연결되었나요 ?  --> "+prodNo);
+		
+		//Business Logic
+		Product product = productService.getProduct(prodNo);
+		
+		// Model 과 View 연결
+		model.addAttribute("product", product);
+		
+		return "forward:/product/getProduct.jsp";
+	}
+	
+	@RequestMapping("/listProduct.do")
+	public String listUser( @ModelAttribute("search") Search search,
+							@RequestParam("menu") String menu,
+							Model model , HttpServletRequest request) throws Exception{
+		
+		System.out.println("/listProduct.do");
+		System.out.println("menu 잘 넘어 왔나요 ?? --->"+menu);
+		
+		if(search.getCurrentPage()==0 ){
+			search.setCurrentPage(1);
+		}
+		search.setPageSize(pageSize);
+		
+		// Business logic 수행
+		Map<String , Object> map=productService.getProductList(search);
+		
+		Page resultPage = new Page( search.getCurrentPage(), ((Integer)map.get("totalCount")).intValue(), pageUnit, pageSize);
+		System.out.println(resultPage);
+		
+		// Model 과 View 연결
+		model.addAttribute("list", map.get("list"));
+		model.addAttribute("resultPage", resultPage);
+		model.addAttribute("search", search);
+		System.out.println("resultpage 잘 세팅되었나요 ??? --->"+resultPage);
+		System.out.println("menu 잘 넘어 왔나요 222?? --->"+menu);
+		System.out.println("list 잘 넘어 왔나요 ?? --->"+map.get("list"));
+
+		if (menu.equals("manage")) {
+			return "forward:/product/listProductManage.jsp";
+		}else {
+			return "forward:/product/listProductSearch.jsp";
+		}
+	}
+		
+	@RequestMapping("/updateProductView.do")
+	public String updateProductView(@RequestParam("prodNo") int prodNo , Model model ) throws Exception{
+
+		System.out.println("/updateProductView.do");
+		//Business Logic
+		Product product = productService.getProduct(prodNo);
+		// Model 과 View 연결
+		model.addAttribute("product", product);
+		
+		return "forward:/product/updateProductView.jsp";
+	}
+
+	
+	@RequestMapping("/updateProduct.do")
+	public String updateProduct(@ModelAttribute("product") Product product,
+								@RequestParam("prodNo") int prodNo,
+								@RequestParam("regdate") Date regdate,
+								Model model , HttpSession session) throws Exception{
+
+		product.setProdNo(prodNo);
+		product.setRegDate(regdate);
+		System.out.println("/updateProduct.do");
+		//Business Logic
+		productService.updateProduct(product);
+		
+		return "forward:/product/updateProduct.jsp";
+	}
+}
